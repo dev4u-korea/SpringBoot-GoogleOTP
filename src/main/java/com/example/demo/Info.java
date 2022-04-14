ad83a44ad3cdc05dc06cff8a19327c76a4110c80
dev4u-korea <moonsun4ux@gmail.com>
2022-04-14 14:12:01 +0900

    최초 커밋

---
+++ src/main/java/com/example/demo/Info.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Info {
+
+  @GetMapping("/version")
+  public String version() {
+    return "ApiServer v0.0.1";
+  }
+
+  @GetMapping("/echo")
+  public String echo(@RequestParam String msg) {
+    return "Response = " + msg;
+  }
+
+  @GetMapping("/version/{idx}")
+  public String version(@PathVariable Long idx) {
+    return "version = " + idx;
+  }
+  
+}
