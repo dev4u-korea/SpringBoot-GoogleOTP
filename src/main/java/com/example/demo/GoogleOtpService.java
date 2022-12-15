2f6ef6c52e79f325d9b91ed6baebf5aa4793a930
dev4u-korea <moonsun4ux@gmail.com>
2022-12-15 11:05:08 +0900

    코드 정리

--- src/main/java/com/example/demo/GoogleOtpService.java
+++ src/main/java/com/example/demo/GoogleOtpService.java
@@ -7,14 +7,13 @@ import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
 import java.io.OutputStream;
 
 @RestController
-public class QRcode {
+public class GoogleOtpService {
 
     @RequestMapping(value = "/qrimage", method = RequestMethod.GET)
-    public void downloadQrImage(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void downloadQrImage(HttpServletRequest request, HttpServletResponse response) {
 
         GoogleOTP otp = new GoogleOTP();
         String userName = request.getParameter("userName");
@@ -23,24 +22,17 @@ public class QRcode {
         String otpauth = String.format("otpauth://totp/%s?secret=%s&issuer=%s", userName, encodedKey, hostName);
 
         // QR CODE 이미지 생성
-        BufferedImage image = null;
+        BufferedImage image;
+        OutputStream os;
         try {
             image = otp.getQRImage(otpauth, "UTF-8",200,200);
+            response.setContentType("image/png");
+            os = response.getOutputStream();
+            ImageIO.write(image, "PNG", os);
+            os.flush();
         } catch (Exception ig) {
             ig.printStackTrace();
         }
-
-        response.setContentType("image/png");
-        OutputStream os = null;
-        try {
-            os = response.getOutputStream();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        ImageIO.write(image, "PNG", os);
-
-        os.flush();
 
     }
 
